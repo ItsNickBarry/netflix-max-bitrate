ef5214a4811b6f37718696779d9b7a8deba567db
Nick Barry <itsnickbarry@protonmail.ch>
2019-03-09 21:57:58 -0600

    initialize repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# Netflix Max Bitrate
+
+Enforce the highest available bitrate for any Netflix title in Firefox.  Works for both audio and video.
+
+This extension requires the hidden Netflix bitrate menu to be active and accessible by the `CTRL` + `ALT` + `SHIFT` + `S` keyboard combination.  As of this publication, Netflix has disabled the bitrate menu, but certain browser extensions have reenabled it.  The [Force 1080p playback for Netflix](https://addons.mozilla.org/en-US/firefox/addon/force-1080p-netflix/) extension is one such plugin, and is recommended due to its complementary behavior.
+
+## FAQ
+
+### I work for Netflix.  How do I throttle the bitrate?
+
+The bitrate is set just after the page is loaded.  At any point afterwards, it may be manually overridden through the hidden bitrate menu.
+
+### I work for Google.  How do I collect your personal data?
+
+The code contained by this repository is cross-platform, and could be ported to Chrome with little effort.  The [Netflix 1080p](https://chrome.google.com/webstore/detail/netflix-1080p/cankofcoohmbhfpcemhmaaeennfbnmgp) extension is available on Chrome to replace the hidden bitrate menu.
+
+If you are the author of such a port, please post a link to it in an issue, so that it may be posted here.
---
+++ netflix_max_bitrate.js
@@ -0,0 +1,80 @@
+let ready = function (fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
+
+let fn = function () {
+  window.dispatchEvent(new KeyboardEvent('keydown', {
+    keyCode: 83,
+    ctrlKey: true,
+    altKey: true,
+    shiftKey: true,
+  }));
+
+  const VIDEO_SELECT = document.evaluate(
+    "//div[text()='Video Bitrate']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null,
+  ).singleNodeValue;
+
+  const AUDIO_SELECT = document.evaluate(
+    "//div[text()='Audio Bitrate']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null,
+  ).singleNodeValue;
+
+  const BUTTON = document.evaluate(
+    "//button[text()='Override']",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null,
+  ).singleNodeValue;
+
+  if (!(VIDEO_SELECT && AUDIO_SELECT && BUTTON)){
+    return false;
+  }
+
+  [VIDEO_SELECT, AUDIO_SELECT].forEach(function (el) {
+    let parent = el.parentElement;
+
+    let options = parent.querySelectorAll('select > option');
+
+    for (var i = 0; i < options.length - 1; i++) {
+      options[i].removeAttribute('selected');
+    }
+
+    options[options.length - 1].setAttribute('selected', 'selected');
+  });
+
+  BUTTON.click();
+
+  return true;
+};
+
+let run = function () {
+  fn() || setTimeout(run, 100);
+};
+
+// ready(run);
+
+run();
+
+
+// ready(function () {
+//   document.addEventListener('keydown', function (event) {
+//     console.log('start');
+//     if (!done &&event.ctrlKey && event.altKey && event.shiftKey && 83 == event.keyCode) {
+//       console.log('okay');
+//       run();
+//     }
+//   });
+//
+// });
